f480a3cfc7b417a79d94336d4377fe75044a6e30
Desmond <desmondaldridge@gmail.com>
2020-11-16 17:33:23 -0600

    Testing the Atlas

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const path = require("path");
 
 //Port
 const PORT = process.env.PORT || 3000;
